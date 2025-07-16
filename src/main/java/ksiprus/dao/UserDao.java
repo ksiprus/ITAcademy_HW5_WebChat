fd450234ae6414a4bf7bbdf411d5198be603248d
Uladzimir Shunevich <ksiprus@mail.ru>
2025-07-16 12:31:05 +0300

    Добавлен класс UserDao с методами для работы с пользователями
    - Реализован метод save(User user) для сохранения нового пользователя в базе данных.
    - Реализован метод findByLogin(String login) для поиска пользователя по логину.
    - Реализован метод findAll() для получения списка всех пользователей из базы данных.
    - Реализован метод count() для подсчета общего количества пользователей в базе данных.
    - Использован DataSourceSingleton для управления соединениями с базой данных.

---
+++ src/main/java/ksiprus/dao/UserDao.java
@@ -0,0 +1,81 @@
+package ksiprus.dao;
+
+import ksiprus.model.User;
+import ksiprus.utils.DataSourceSingleton;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDao {
+    public void save(User user) throws SQLException {
+        String sql = "INSERT INTO users (login, password, name, birth_date, reg_date, role) VALUES (?, ?, ?, ?, ?, ?)";
+        try (Connection conn = DataSourceSingleton.getInstance().getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, user.getLogin());
+            ps.setString(2, user.getPassword());
+            ps.setString(3, user.getName());
+            ps.setDate(4, Date.valueOf(user.getBirthDate()));
+            ps.setTimestamp(5, Timestamp.valueOf(user.getRegDate()));
+            ps.setString(6, user.getRole());
+            ps.executeUpdate();
+        }
+    }
+
+    public User findByLogin(String login) throws SQLException {
+        String sql = "SELECT * FROM users WHERE login = ?";
+        try (Connection conn = DataSourceSingleton.getInstance().getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, login);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return new User.Builder()
+                        .id(rs.getInt("id"))
+                        .login(rs.getString("login"))
+                        .password(rs.getString("password"))
+                        .name(rs.getString("name"))
+                        .birthDate(rs.getDate("birth_date").toLocalDate())
+                        .regDate(rs.getTimestamp("reg_date").toLocalDateTime())
+                        .role(rs.getString("role"))
+                        .build();
+            }
+            return null;
+        }
+    }
+
+    // Метод для получения всех пользователей
+    public List<User> findAll() throws SQLException {
+        List<User> users = new ArrayList<>();
+        String sql = "SELECT * FROM users";
+        try (Connection conn = DataSourceSingleton.getInstance().getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                User user = new User.Builder()
+                        .id(rs.getInt("id"))
+                        .login(rs.getString("login"))
+                        .password(rs.getString("password"))
+                        .name(rs.getString("name"))
+                        .birthDate(rs.getDate("birth_date").toLocalDate())
+                        .regDate(rs.getTimestamp("reg_date").toLocalDateTime())
+                        .role(rs.getString("role"))
+                        .build();
+                users.add(user);
+            }
+        }
+        return users;
+    }
+
+    // Метод для подсчета пользователей
+    public int count() throws SQLException {
+        String sql = "SELECT COUNT(*) FROM users";
+        try (Connection conn = DataSourceSingleton.getInstance().getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+        }
+        return 0;
+    }
+}
