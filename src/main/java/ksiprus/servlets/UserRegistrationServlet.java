c312af235649bf02e40ca9f949bc96f72bb3c102
Uladzimir Shunevich <ksiprus@mail.ru>
2025-07-16 16:06:15 +0300

    Добавлен UserRegistrationServlet для обработки регистрации пользователей

---
+++ src/main/java/ksiprus/servlets/UserRegistrationServlet.java
@@ -0,0 +1,32 @@
+package ksiprus.servlets;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import ksiprus.service.UserService;
+
+import java.io.IOException;
+
+@WebServlet("/api/user")
+public class UserRegistrationServlet {
+    private final UserService service = new UserService();
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+        String name = req.getParameter("name");
+        String birthDate = req.getParameter("birthDate");
+
+        try {
+            service.register(login, password, name, birthDate);
+            resp.sendRedirect(req.getContextPath() + "/signIn.jsp");
+        } catch (Exception e) {
+            req.setAttribute("Ошибка!", e.getMessage());
+            req.getRequestDispatcher("ui/signUp.jsp").forward(req, resp);
+
+        }
+    }
+}
